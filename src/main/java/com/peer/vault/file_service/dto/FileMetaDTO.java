4a1c8cdf1a90ef44cd23403202c1ab4670dd2806
Manish Kumar Choudhary <107823673+ManishKumarChoudhary2003@users.noreply.github.com>
2024-09-07 14:06:12 +0530

    Update FileMetaDTO.java

--- src/main/java/com/peer/vault/file_service/dto/FileMetaDTO.java
+++ src/main/java/com/peer/vault/file_service/dto/FileMetaDTO.java
@@ -4,8 +4,14 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+
 @Data
 public class FileMetaDTO {
+
+    private Long id;
 
     private String fileName;
 
@@ -14,7 +20,13 @@ public class FileMetaDTO {
     @JsonIgnore
     private MultipartFile file;
 
+    private String fileSize;
+
     private Long userId;
 
+    private String CID;
+
     private String description;
+
+    private LocalDateTime uploadDate;
 }
